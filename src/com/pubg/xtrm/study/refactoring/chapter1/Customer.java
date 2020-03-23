f91a65bd1fa1965949076231b8e72dea23b9a112
isbee <zidane0428@gmail.com>
2020-03-23 21:27:51 +0900

    First commit

---
+++ src/com/pubg/xtrm/study/refactoring/chapter1/Customer.java
@@ -0,0 +1,64 @@
+package com.pubg.xtrm.example1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+    private String name;
+    private List<Rental> rentals;
+
+    public Customer(String name) {
+        this.name = name;
+        rentals = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        String result = "Rental Record for " + getName() + "\n";
+        for (Rental rental : rentals) {
+            double thisAmount = 0;
+
+            // 각 영화에 대한 요금 결정
+            switch (rental.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (rental.getDaysRented() > 2) {
+                        thisAmount += (rental.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += rental.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDREN:
+                    thisAmount += 1.5;
+                    if (rental.getDaysRented() > 3) {
+                        thisAmount += (rental.getDaysRented() - 3) * 1.5;
+                    }
+                    break;
+            }
+            // 포인트(frequent renter points) 추가
+            frequentRenterPoints++;
+            // new release를 2일 이상 대여하는 경우 추가 포인트 제공
+            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
+
+            // 이 대여에 대한 요금 계산결과 표시
+            result += "\t" + rental.getMovie().getTitle() + "\t" + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+        // footer 추가
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + "frequent renter points";
+        return result;
+    }
+}
---
+++ src/com/pubg/xtrm/study/refactoring/chapter1/Movie.java
@@ -0,0 +1,31 @@
+package com.pubg.xtrm.example1;
+
+public class Movie {
+    public static final int CHILDREN = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String title;
+    private int priceCode;
+
+    public Movie(String title, int priceCode) {
+        this.title = title;
+        this.priceCode = priceCode;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPriceCode(int priceCode) {
+        this.priceCode = priceCode;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPriceCode() {
+        return priceCode;
+    }
+}
