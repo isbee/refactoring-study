e6c861afc1c2b068e05836f59c93cfc7bf2a6886
isbee <zidane0428@gmail.com>
2020-04-01 16:58:14 +0900

    Replace method with 'Method Object'

---
+++ src/com/pubg/xtrm/study/refactoring/chapter6/Account.java
@@ -0,0 +1,18 @@
+package com.pubg.xtrm.study.refactoring.chapter6;
+
+public class Account {
+
+    int gamma(int inputVal, int quantity, int yearToDate) {
+        int importantValue1 = (inputVal * quantity) + delta();
+        int importantValue2 = (inputVal * yearToDate) + 100;
+        if ((yearToDate - importantValue1) > 100) {
+            importantValue2 -= 20;
+        }
+        int importantValue3 = importantValue2 * 7;
+        return importantValue3 - 2 * importantValue1;
+    }
+
+    int delta() {
+        return 0;
+    }
+}
