8aadb310467dde7650826afe17bbe412b36e22a7
isbee <zidane0428@gmail.com>
2020-04-01 16:20:44 +0900

    Replace temp with query

--- src/com/pubg/xtrm/study/refactoring/chapter6/Order.java
+++ src/com/pubg/xtrm/study/refactoring/chapter6/Order.java
@@ -2,7 +2,21 @@ package com.pubg.xtrm.study.refactoring.chapter6;
 
 public class Order {
 
+    private int quantity;
+    private double itemPrice;
+
     public double getAmount() {
         return 1.0;
+    }
+
+    double getPrice() {
+        double basePrice = quantity * itemPrice;
+        double discountFactor;
+        if (basePrice > 1000) {
+            discountFactor = 0.95;
+        } else {
+            discountFactor = 0.98;
+        }
+        return basePrice * discountFactor;
     }
 }
