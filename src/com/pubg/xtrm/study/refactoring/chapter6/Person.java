5336618e590f1df754b888a928d4eb11181061d9
isbee <zidane0428@gmail.com>
2020-04-01 17:12:00 +0900

    Substitue algorithm

--- src/com/pubg/xtrm/study/refactoring/chapter6/Person.java
+++ src/com/pubg/xtrm/study/refactoring/chapter6/Person.java
@@ -1,17 +1,16 @@
 package com.pubg.xtrm.study.refactoring.chapter6;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Person {
 
     String foundPerson(String[] people) {
+        List candidates = Arrays.asList(new String[]{"Don", "John", "Kent"});
         for (int i = 0; i < people.length; i++) {
-            if (people[i].equals("Don")) {
-                return "Don";
-            }
-            if (people[i].equals("John")) {
-                return "John";
-            }
-            if (people[i].equals("Kent")) {
-                return "Kent";
+            if (candidates.contains(people[i])) {
+                return people[i];
             }
         }
         return "";
