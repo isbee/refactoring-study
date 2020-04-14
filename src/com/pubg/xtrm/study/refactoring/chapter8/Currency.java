8459689cb36f773559eddebe6378cb4d87275438
Seunghyun Lee <seunghyun@pubg.com>
2020-04-14 11:24:17 +0900

    Change reference to value

--- src/com/pubg/xtrm/study/refactoring/chapter8/Currency.java
+++ src/com/pubg/xtrm/study/refactoring/chapter8/Currency.java
@@ -1,7 +1,16 @@
 package com.pubg.xtrm.study.refactoring.chapter8;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Currency {
 
+    private static Map<String, Currency> currencyList = new HashMap<>(){
+        {
+            put("USD", new Currency("USD"));
+            put("KRW", new Currency("KRW"));
+        }
+    };
     private String code;
 
     public String getCode() {
@@ -12,7 +21,12 @@ public class Currency {
         this.code = code;
     }
 
+    public static Currency getCurrencyByCode(String code) {
+        return currencyList.get(code);
+    }
+
     public static void main(String[] args) {
         new Currency("USD").equals(new Currency("USD"));   // false
+        Currency c = getCurrencyByCode("USD");   // reference object
     }
 }
