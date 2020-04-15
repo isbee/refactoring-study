bf6f2f1f70e5e065ca9beb5874390c11a0ec0840
Seunghyun Lee <seunghyun@pubg.com>
2020-04-15 19:28:05 +0900

    Replace Type Code with Class

--- src/com/pubg/xtrm/study/refactoring/chapter8/Person.java
+++ src/com/pubg/xtrm/study/refactoring/chapter8/Person.java
@@ -2,22 +2,24 @@ package com.pubg.xtrm.study.refactoring.chapter8;
 
 public class Person {
 
-    public static final int O = 0;
-    public static final int A = 1;
-    public static final int B = 2;
-    public static final int AB = 3;
+    public static final int O = BloodGroup.O.getCode();
+    public static final int A = BloodGroup.A.getCode();
+    public static final int B = BloodGroup.B.getCode();
+    public static final int AB = BloodGroup.AB.getCode();
 
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
 
+    // e.g. Person person = new Person(BloodGroup.A);
     public Person(int bloodGroup) {
-        this.bloodGroup = bloodGroup;
+        this.bloodGroup = BloodGroup.code(bloodGroup);
     }
 
+    // e.g. person.setBloodGroup(BloodGroup.AB)
     public void setBloodGroup(int arg) {
-        bloodGroup = arg;
+        this.bloodGroup = BloodGroup.code(arg);
     }
 
     public int getBloodGroup() {
-        return bloodGroup;
+        return bloodGroup.getCode();
     }
 }
