312f5de0dc4e0127cabf714631418bc2d0b2f313
Seunghyun Lee <seunghyun@pubg.com>
2020-04-15 16:39:55 +0900

    Change Unidirectional Association to Bidirectional

---
+++ src/com/pubg/xtrm/study/refactoring/chapter8/Order2.java
@@ -0,0 +1,14 @@
+package com.pubg.xtrm.study.refactoring.chapter8;
+
+public class Order2 {
+
+    Customer customer;
+
+    Customer getCustomer() {
+        return customer;
+    }
+
+    void setCustomer(Customer arg) {
+        customer = arg;
+    }
+}
