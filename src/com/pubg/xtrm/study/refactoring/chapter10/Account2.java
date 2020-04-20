09081ec79368179a8e7174bfecf5ec563d569423
Seunghyun Lee <seunghyun@pubg.com>
2020-04-20 21:13:28 +0900

    Replace Error Code with Exception

--- src/com/pubg/xtrm/study/refactoring/chapter10/Account2.java
+++ src/com/pubg/xtrm/study/refactoring/chapter10/Account2.java
@@ -8,13 +8,11 @@ public class Account2 {
         this.balance = balance;
     }
 
-    void withdraw(int amount) {
+    void withdraw(int amount) throws BalanceException {
         if (!canWithdraw(amount)) {
-            // handleOverdrawn();
-
-            // IllegalArgumentException은 RuntimeException의 하위 클래스이므로
-            // unchecked exception 이다.
-            throw new IllegalArgumentException("Amount too large");
+            // BalanceException은 Exception의 하위 클래스이므로
+            // 컴파일 단계에서 확인되는 checked exception 이다.
+            throw new BalanceException();
         }
         balance -= amount;
     }
@@ -22,8 +20,12 @@ public class Account2 {
     public static void main(String[] args) {
         Account2 account2 = new Account2(100);
         int amount = 200;
-        account2.withdraw(amount);
-        // doTheUsualThing();
+        try {
+            account2.withdraw(amount);
+            // doTheUsualThing();
+        } catch (BalanceException e) {
+            // handleOverdrawn();
+        }
     }
 
     boolean canWithdraw(int amount) {
---
+++ src/com/pubg/xtrm/study/refactoring/chapter10/BalanceException.java
@@ -0,0 +1,4 @@
+package com.pubg.xtrm.study.refactoring.chapter10;
+
+public class BalanceException extends Exception {
+}
