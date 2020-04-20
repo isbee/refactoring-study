209d842b1014a5462e4d41c5c0ce9bee3495d5fe
Seunghyun Lee <seunghyun@pubg.com>
2020-04-20 09:35:59 +0900

    Seperate Query from Modifier

--- src/com/pubg/xtrm/study/refactoring/chapter10/Security.java
+++ src/com/pubg/xtrm/study/refactoring/chapter10/Security.java
@@ -3,18 +3,30 @@ package com.pubg.xtrm.study.refactoring.chapter10;
 public class Security {
 
     void checkSecurity(String[] people) {
-        String found = foundMiscreant(people);
+        sendAlert(people);
+        String found = foundPerson(people);
         // someLaterCode(found);
     }
 
-    String foundMiscreant(String[] people) {
+    void sendAlert(String[] people) {
         for (int i = 0; i < people.length; i++) {
             if (people[i].equals("Don")) {
                 // sendAlert();
-                return "Don";
+                return;
             }
             if (people[i].equals("John")) {
                 // sendAlert();
+                return;
+            }
+        }
+    }
+
+    String foundPerson(String[] people) {
+        for (int i = 0; i < people.length; i++) {
+            if (people[i].equals("Don")) {
+                return "Don";
+            }
+            if (people[i].equals("John")) {
                 return "John";
             }
         }
